fe7316798b9a7de7669a4a574bbfb164f4941239
Przemek Jarek <kasztan0803@gmail.com>
2023-08-21 23:39:38 +0200

    Added numbers

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ console.log(divs);
 
 for (let i = 0; i < divs.length; i++) {
     divs[i].addEventListener("click", function(){
-        divs[i].innerText = "ty chuju " + i
+        divs[i].innerHTML = "zmiana <br>" + i
+        document.write("ojjj")
     })
+}
+
+var liczby = document.getElementById("liczby")
+for (let i = -111; i < 112; i++){
+liczby.innerHTML += "Liczba : " + i + "<br>"
+   
 }
